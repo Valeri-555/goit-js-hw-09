f1eb4d4507b586db7e1e3c9e8f67cccaf5ca2e6b
Valeri-555 <Lelia.valeria2104@gmail.com>
2024-02-08 23:13:47 +0200

    created files

---
+++ src/js/2-form.js
@@ -0,0 +1,60 @@
+    
+
+const STORAGE_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+const textarea = form.querySelector('textarea');
+
+form.addEventListener('input', onFormInput);
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  const email = form.elements.email.value;
+  const message = form.elements.message.value;
+
+  const data = {
+    email,
+    message,
+  };
+
+  console.log(data);
+
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
+};
+
+function onFormInput() {
+  const email = form.elements.email.value;
+  const message = form.elements.message.value;
+
+  const data = {
+   email,
+    message,
+  };
+
+  saveToLS(STORAGE_KEY, data);
+};
+
+function saveToLS(key, value) {
+  const zip = JSON.stringify(value);
+  localStorage.setItem(key, zip);
+};
+
+function loadFromLS(key) {
+  const storKey = localStorage.getItem(key);
+  try {
+    return JSON.parse(zip);
+  } catch {
+    return storKey;
+  }
+};
+
+function init() {
+  const data = loadFromLS(STORAGE_KEY) || {};
+  form.elements.email.value = data.email || '';
+  form.elements.message.value = data.message || '';
+};
+
+init();
